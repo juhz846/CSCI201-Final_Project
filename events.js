bcd059ce3c30e2d7c8a35e3b60ea20be333afed3
Aarav Monga <armonga@usc.edu>
2024-04-26 23:15:48 -0700

    Update events.js

--- events.js
+++ events.js
@@ -1,38 +1,42 @@
-function goToEventPage() {
-    //var eventName = document.getElementById("searchbox").value;
-    //ventName = eventName.replace(/\s+/g, '');
-    
-    // Check if the event exists
-    //if (!checkEventExists(eventName)) {
-     //   alert("Event not found. Please try again.");
-    //} 
-    //else {
-			
-    $.ajax({
-	type: "GET",
-	url: "EventDisplayServlet",
-	data: {
-		search:$("#searchbox").val()
-	},
-	success: (result)=>{
-		var events = document.getElementById("events");
-		const jsonArray = result.split("*");
-		console.log(jsonArray)
-		if (jsonArray == ''){
-			events.innerHTML = "no data to display!";
-		} else {
-			events.innerHTML = "";
-			for (const jsonObj of jsonArray) {
-				console.log(jsonArray)
-				const json = JSON.parse(jsonObj)
-				events.innerHTML += '<div class="row"><div class="event-card"><br><img src= "'+json.img_file+'" width = "200" height = "200"'+
-	    'alt = "'+json.name+'"><div id="description"><div><h3><a onclick="goToEventPage(\''+json.name+'\')">'+json.name+'</a></h3>' +
-	    '<p>'+json.event_date+'</p></div><div id="right"><p>$'+json.price+'</p><p>'+json.city+", "+json.state+'</p></div></div></div></div>'				
+function goToEventPage(eventName) {
+            //var eventName = document.getElementById("searchbox").value;
+            //ventName = eventName.replace(/\s+/g, '');
+            
+            // Check if the event exists
+            //if (!checkEventExists(eventName)) {
+             //   alert("Event not found. Please try again.");
+            //} 
+            //else {
+	window.location.href = "event.html?eventName="+eventName;
+            
+            //}
+}
+
+function search(){
+	$.ajax({
+		type: "GET",
+		url: "EventDisplayServlet",
+		data: {
+			search:$("#searchbox").val()
+		},
+		success: (result)=>{
+			var events = document.getElementById("events");
+			const jsonArray = result.split("*");
+			console.log(jsonArray)
+			if (jsonArray == ''){
+				events.innerHTML = "no data to display!";
+			} else {
+				events.innerHTML = "";
+				for (const jsonObj of jsonArray) {
+					console.log(jsonArray)
+					const json = JSON.parse(jsonObj)
+					events.innerHTML += '<div class="row"><div class="event-card"><br><img src= "'+json.img_file+'" width = "200" height = "200"'+
+		    'alt = "'+json.name+'"><div id="description"><div><h3><a onclick="goToEventPage(\''+json.name+'\')">'+json.name+'</a></h3>' +
+		    '<p>'+json.event_date+'</p></div><div id="right"><p>$'+json.price+'</p><p>'+json.city+", "+json.state+'</p></div></div></div></div>'				
+				}
 			}
 		}
-	}
 	})
-    //}
 }
 
 function generateContent(){
